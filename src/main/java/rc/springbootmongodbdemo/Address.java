0a39db60393ce07d62e7f352242f0700a4e40a44
unknown <minhtrung511081@gmail.com>
2021-06-22 19:47:46 +0700

    initial commit

---
+++ src/main/java/rc/springbootmongodbdemo/Address.java
@@ -0,0 +1,23 @@
+package rc.springbootmongodbdemo;
+
+//import org.springframework.data.mongodb.core.aggregation.ArithmeticOperators;
+
+public class Address {
+    private String city;
+    private String country;
+
+    protected Address(){}
+
+    public Address(String city, String country){
+        this.city = city;
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}
---
+++ src/main/java/rc/springbootmongodbdemo/HotelController.java
@@ -0,0 +1,63 @@
+package rc.springbootmongodbdemo;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/hotels")
+public class HotelController {
+    private HotelRepository hotelRepository;
+
+    public HotelController(HotelRepository hotelRepository){
+        this.hotelRepository = hotelRepository;
+    }
+
+    @GetMapping("/all")
+    public List<Hotel> getAll(){
+        List<Hotel>  hotels = this.hotelRepository.findAll();
+
+        return hotels;
+    }
+
+    @PutMapping
+    public void insert(@RequestBody Hotel hotel){
+        this.hotelRepository.insert(hotel);
+    }
+
+    @PostMapping
+    public void update(@RequestBody Hotel hotel){
+        this.hotelRepository.save(hotel);
+    }
+
+
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") String id){
+        this.hotelRepository.deleteById(id);
+    }
+
+    @GetMapping("/{id}")
+    public Hotel getById(@PathVariable("id") String id){
+        Hotel hotel = this.hotelRepository.	findAllById(id);
+
+        return hotel;
+    }
+
+    @GetMapping("/price/{maxPrice}")
+    public List<Hotel> getByPricePerNight(@PathVariable("maxPrice") int maxPrice){
+        List<Hotel> hotels = this.hotelRepository.findByPricePerNightLessThan(maxPrice);
+
+        return hotels;
+    }
+
+    @GetMapping("/address/{city}")
+    public List<Hotel> getByCity(@PathVariable("city") String city){
+        List<Hotel> hotels = this.hotelRepository.findByCity(city);
+
+        return hotels;
+    }
+
+
+
+}
